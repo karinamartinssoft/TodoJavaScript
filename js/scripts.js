5d02808904ddb65fa12390f47da9100f11efc5e9
karinamartinssoft <karina.martins@softniels.com.br>
2023-06-06 08:27:45 -0300

    Conclusão do Projeto

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
-let oldInputValue;
+let oldInputValue = "";
 
 // Funções
 const saveTodo = (text) => {
